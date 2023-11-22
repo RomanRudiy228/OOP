15845d72cc6510f703a5e2528d5a077a0ea32983
Roman Rudiy <140906123+RomanRudiy228@users.noreply.github.com>
2023-11-22 16:16:32 +0200

    Add files via upload

---
+++ OOPLAB№4/Interface.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OOPLAB_4.Doctor;
+
+namespace OOPLAB_4
+{
+    internal interface _
+    {
+        interface IDateAndCopy
+        {
+            object DeepCopy();
+            DateTime Date { get; set; }
+        }
+
+        class Person : IDateAndCopy
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public DateTime Date { get; set; }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+                Date = DateTime.Now;
+            }
+
+            public Person()
+            {
+                Name = "Yarik Pchel";
+                Age = 30;
+                Date = DateTime.Now;
+            }
+
+            public virtual bool Equals(object obj)
+            {
+                if (obj is Person other)
+                {
+                    return Name == other.Name && Age == other.Age;
+                }
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Name, Age);
+            }
+
+            public virtual object DeepCopy()
+            {
+                return new Person(Name, Age);
+            }
+        }
+
+        class Chair : IDateAndCopy
+        {
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+
+            public Chair(string name)
+            {
+                Name = name;
+                Date = DateTime.Now;
+            }
+
+            public Chair()
+            {
+                Name = "Default Chair";
+                Date = DateTime.Now;
+            }
+
+            public object DeepCopy()
+            {
+                return new Chair(Name);
+            }
+        }
+
+        class Section : IDateAndCopy
+        {
+            public string Name { get; set; }
+            public bool IsDepartment { get; set; }
+            public DateTime Date { get; set; }
+
+            public Section(string name, bool isDepartment)
+            {
+                Name = name;
+                IsDepartment = isDepartment;
+                Date = DateTime.Now;
+            }
+
+            public Section()
+            {
+                Name = "Default Section";
+                IsDepartment = false;
+                Date = DateTime.Now;
+            }
+
+            public override string ToString()
+            {
+                return $"Name: {Name}, IsDepartment: {IsDepartment}, Date: {Date.ToShortDateString()}";
+            }
+
+            public object DeepCopy()
+            {
+                return new Section(Name, IsDepartment);
+            }
+        }
+
+        class Employees : Person
+        {
+            private Education education;
+            private int age;
+            private ArrayList sections;
+            private ArrayList chairs;
+
+            public Employees(Person person, Education education, int age) : base(person.Name, person.Age)
+            {
+                this.education = education;
+                this.age = age;
+                sections = new ArrayList();
+                chairs = new ArrayList();
+            }
+
+            public Employees() : base()
+            {
+                education = Education.PhD;
+                age = 1;
+                sections = new ArrayList();
+                chairs = new ArrayList { new Chair() };
+            }
+
+            public Person PersonData
+            {
+                get { return new Person(Name, Age); }
+                set { Name = value.Name; Age = value.Age; }
+            }
+
+            public double AverageAge
+            {
+                get { return chairs.Count > 0 ? (double)age / chairs.Count : 0; }
+            }
+
+            public ArrayList Sections
+            {
+                get { return sections; }
+                set { sections = value; }
+            }
+
+            public ArrayList Chairs
+            {
+                get { return chairs; }
+                set { chairs = value; }
+            }
+
+            public void AddChairs(params Chair[] newChairs)
+            {
+                chairs.AddRange(newChairs);
+            }
+
+            public override string ToString()
+            {
+                return $"Person Data: {PersonData.Name}, Age: {PersonData.Age}, Education: {education}, Average Age: {AverageAge}, Sections: {string.Join(", ", Sections.ToArray())}, Chairs: {string.Join(", ", Chairs.ToArray())}";
+            }
+           
+            public override object DeepCopy()
+            {
+                Employees copy = new Employees(PersonData, education, age);
+                copy.sections = new ArrayList((ICollection)sections.Cast<Section>().Select(s => s.DeepCopy()));
+                copy.chairs = new ArrayList((ICollection)chairs.Cast<Chair>().Select(c => c.DeepCopy()));
+                return copy;
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return sections.Cast<object>().Concat(chairs.Cast<object>()).GetEnumerator();
+            }
+
+            public IEnumerable GetEnumerator(double maxAge)
+            {
+                return chairs.Cast<Chair>().Where(c => c.Date > DateTime.Now.AddYears((int)-maxAge));
+            }
+        }
+
+    }
+}
---
+++ OOPLAB№4/Program.cs
@@ -0,0 +1,63 @@
+﻿using OOPLAB_4;
+using System.Collections;
+using static OOPLAB_4._;
+using static OOPLAB_4.Doctor;
+using Person = OOPLAB_4._.Person;
+
+try
+        {
+            Person person1 = new Person("John", 25);
+            Person person2 = new Person("John", 25);
+
+            Console.WriteLine($"Are persons equal? {person1.Equals(person2)}");
+            Console.WriteLine($"Hash code of person1: {person1.GetHashCode()}");
+            Console.WriteLine($"Hash code of person2: {person2.GetHashCode()}");
+
+            Employees employees = new Employees(person1, Doctor.Education.PhD, 30);
+            employees.AddChairs(new Chair("Chair A"), new Chair("Chair B"));
+            employees.Sections.Add(new Section("Section X", true));
+            employees.Sections.Add(new Section("Section Y", false));
+
+            Console.WriteLine("\nEmployee Information:");
+            Console.WriteLine(employees.ToString());
+
+            Console.WriteLine("\nPerson Information (from Employee):");
+            Console.WriteLine(employees.PersonData.ToString());
+
+            Console.WriteLine("\nDeep Copy and Modification:");
+            Employees copy = (Employees)employees.DeepCopy();
+            copy.PersonData.Name = "Jane";
+            copy.Sections.Add(new Section("Section Z", true));
+            copy.Chairs.Add(new Chair("Chair C"));
+            Console.WriteLine("Original Employee:");
+            Console.WriteLine(employees.ToString());
+            Console.WriteLine("Modified Copy:");
+            Console.WriteLine(copy.ToString());
+
+            Console.WriteLine("\nInvalid Age Assignment (Exception Handling):");
+            try
+            {
+                employees.Age = 25; // This will throw an exception
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+
+            Console.WriteLine("\nIterating through Sections and Chairs:");
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nIterating through Chairs with Age > 35:");
+            foreach (Chair chair in employees.GetEnumerator(35))
+            {
+                Console.WriteLine(chair);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unexpected Exception: {ex.Message}");
+        }
+    
